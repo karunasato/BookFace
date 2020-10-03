f11450a16af9d5f574d740ac5da2d3007856f587
Mariana Robertson <marygmaciel@gmail.com>
2020-10-03 15:49:02 -0700

    updated config

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "book_club"
+    database: "study_group"
   })
 };
 
