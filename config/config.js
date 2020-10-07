b57d2ed1fa07049e340aec6c8da0b9aaddfddfa3
faranak Sabet <faranaksabet@yahoo.com>
2020-10-06 18:09:57 -0700

    adding cenv to config.js sequelize

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "bm39a8f5w4gv73jo",
-    password: "b31bfasl42jty8sm",
-    database: "wychdah450qu4bos",
-    host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    username: process.env.sqluser,
+    password: process.env.sqlpw,
+    database: process.env.sqldatabase,
+    host: process.env.sqlhost,
     dialect: "mysql",
   },
 };
